36623be20d596e93d7f10f6dfa0edca1dd01d214
MonkaData <110202753+MonkaData@users.noreply.github.com>
2024-10-01 20:40:30 +0200

    Sous surveillance maximale !!

--- ast.ml
+++ ast.ml
@@ -1,4 +1,5 @@
 (* Définition des expressions et des opérateurs *)
+(* D'après un individu peu fiable il y a de la RECURSION GAUCHE*)
 type expr =
   | Val of int                              
   | Add of expr * expr                     
