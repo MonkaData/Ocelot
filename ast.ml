f739c235ded9f6c32c8fa2da5e433af822299f92
MonkaData <110202753+MonkaData@users.noreply.github.com>
2024-11-06 11:51:06 +0100

    Update ast.ml

--- ast.ml
+++ ast.ml
@@ -1,8 +1,4 @@
 (* Définition des expressions et des opérateurs *)
 (* D'après un individu peu fiable il y a de la RECURSION GAUCHE*)
 type expr =
-  | Val of int                              
-  | Add of expr * expr                     
-  | Sub of expr * expr                     
-  | Mul of expr * expr                      
-  | Div of expr * expr                      
+
