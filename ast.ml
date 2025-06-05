df0c08f879a4e78dc7016301bb470fbb17538d5a
MonkaData <110202753+MonkaData@users.noreply.github.com>
2025-06-05 23:30:19 +0200

    Handle list types in C type translation

--- ast.ml
+++ ast.ml
@@ -19,12 +19,23 @@ type operateur_binaire =
 type operateur_unaire =
   | Non
 
+(* Motifs pour le pattern matching *)
+type pattern =
+  | PInt of int
+  | PBool of bool
+  | PVar of ident
+  | PWildcard
+  | PNil
+  | PCons of pattern * pattern
+
 type expr =
   | Entier of int
   | Booleen of bool
   | Variable of ident
   | Binop of operateur_binaire * expr * expr
   | Unop of operateur_unaire * expr
+  | Liste of expr list                      (* [e1; e2; ...] list literals *)
+  | Cons of expr * expr                     (* e1 :: e2 *)
   | Si of expr * expr * expr option   (* Si condition alors expr1 [sinon expr2] *)
   | Let of bool * (ident * expr) list * expr (* let [rec] (id1 = expr1 and id2 = expr2 ...) in expr_in *)
   | Fonction of ident * expr            (* fonction anonyme : fun id -> expr *)
@@ -32,6 +43,7 @@ type expr =
   | TantQue of expr * expr              (* while condition do expr *)
   | Pour of ident * expr * expr * expr   (* for id = expr1 to expr2 do expr *)
   | Sequence of expr list               (* séquence d'expressions séparées par ";" *)
+  | Match of expr * (pattern * expr) list  (* match expr with ... *)
 
 type fonction = {
   nom : ident;
